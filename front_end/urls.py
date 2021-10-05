b00929212f646a3f52834b7147f14b2b3a8f3de8
conscience99 <conscienceekh@gmail.com>
2021-10-05 09:56:46 +0000

    fixed conflict with frontend and backend routing

--- front_end/urls.py
+++ front_end/urls.py
@@ -2,8 +2,7 @@ from django.urls import path, re_path
 from .views import index
 
 urlpatterns = [
-    path('', index),
-    path('/<path:path>', index),
-    path(r'^(?P<path>.*)/$', index)
+    path('',index),
+    re_path(r'^(?P<path>.*)/$', index)
     
 ]
