df00e188f381d24652705bc0813fca1f69eee30e
conscience99 <conscienceekh@gmail.com>
2021-11-12 10:41:17 +0100

    bug fix

--- api/views.py
+++ api/views.py
@@ -217,9 +217,9 @@ class AddLyricsView(APIView):
 
 class SingleLyricsView(APIView):
     def post(self, request, *args, **kwargs ):
-        artist = request.data['artist'].capitalize().strip().replace("-"," ")
+        artist = request.data['artist'].strip().replace("-"," ").title()
         print(artist)
-        title=request.data['title'].capitalize().strip().replace("-"," ")
+        title=request.data['title'].strip().replace("-"," ").title()
         print(title)
         search_history=SearchHistory()
         ### Record activities ###
